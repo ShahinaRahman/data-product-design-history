c71ea1ffede8245cf4d337171ba43258541734d0
Shahina Rahman <shahinarahman101@gmail.com>
2022-10-24 19:56:29 +0100

    Initialize x-govify

---
+++ .eleventy.js
@@ -0,0 +1,46 @@
+module.exports = function (eleventyConfig) {
+  // Options to customise the appearance of your design history
+  // https://x-govuk.github.io/govuk-eleventy-plugin/options/
+  eleventyConfig.addPlugin(require('govuk-eleventy-plugin'), {
+    brandColour: '#28a',
+    fontFamily: 'system-ui, sans-serif',
+    stylesheets: [
+      '/styles/application.css'
+    ],
+    headingPermalinks: true,
+    homeKey: 'X-GOVUK',
+    header: {
+      organisationLogo: 'x-govuk',
+      organisationName: 'X-GOVUK',
+      productName: 'Design history',
+      search: {
+        indexPath: '/search.json',
+        sitemapPath: '/sitemap'
+      }
+    },
+    footer: {
+      contentLicence: {
+        html: 'An unofficial community project. <a class="govuk-footer__link" href="https://github.com/x-govuk/x-govuk.github.io">GitHub source</a>.'
+      },
+      copyright: {
+        text: '© X-GOVUK'
+      }
+    }
+  })
+
+  // Passthrough
+  eleventyConfig.addPassthroughCopy({ './app/images': '.' })
+
+  // Config
+  return {
+    dataTemplateEngine: 'njk',
+    htmlTemplateEngine: 'njk',
+    markdownTemplateEngine: 'njk',
+    dir: {
+      input: 'app',
+      output: 'public',
+      layouts: '_layouts',
+      includes: '_components'
+    }
+  }
+}
